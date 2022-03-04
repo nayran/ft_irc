464bd0f962863fd53a112d706f4d58104e2ded6a
nayran <nayranziebell@icloud.com>
2022-03-03 21:22:21 -0300

    started documentation of what will be used

--- src/ft_irc.hpp
+++ src/ft_irc.hpp
@@ -3,6 +3,27 @@
 
 #include <string>
 #include <iostream>
+#include <sys/socket.h>
 
+
+/*
+ *		FT_IRC
+ *			Socket: permitem comunicacao entre computadores, usam FD para isso.
+ *			         int socket(int domain, int type, int protocol);
+ *				- Domain: define a familia de protocolos
+ *				- Type: especifica a semantica da comunicacao
+ *				- Protocol: especifico para cada tipo de manipulacao de dados
+ *				Essas variaveis serao definidas pela resposta do getaddrinfo
+ *			Setsockopt: 
+ *					int setsockopt(int sockfd, int level, int optname,
+ *							const void *optval, socklen_t optlen);
+ *			Getaddrinfo:
+ *					int getaddrinfo(const char *node, const char *service,
+ *							const struct addrinfo *hints, struct addrinfo **res);	
+ *
+ *
+ *		https://www.linuxhowtos.org/C_C++/socket.htm
+ *		https://www.man7.org/linux/man-pages/man2/socket.2.html
+ */
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 int _err (std::string s)
 {
 	std::cout << s << std::endl;
-	return (0);
+	return (1);
 }
 
 int main(int argc, char **argv)
 {
+	int socketfd;
 	if (argc != 3)
 		return (_err("server port and password expected!"));
-	std::cout << "server port: " << argv[1] << std::endl;
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socketfd < 0)
+		return (_err("error creating socket!"));
+	std::cout << "socketfd: " << socketfd << "\nserver port: " << argv[1] << std::endl;
 	return (0);
 }
