059f5c42953ccfbb8926cbe44be3df9a70822efc
Nayran Ziebell <nayranziebell@icloud.com>
2022-07-11 10:59:56 -0300

    useful link

--- src/serverClass.cpp
+++ src/serverClass.cpp
@@ -157,6 +157,7 @@ void Server::init2()
 	}
 }
 
+// https://stackoverflow.com/questions/10981790/unix-epoll-catch-ctrld-and-ctrlc-in-server
 int Server::receiveMessage(int clifd)
 {
 	char buff = '\0';
